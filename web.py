c452c3ba4d29de09d6a0ea07ac06c4fefc9097cd
HKledor <kledor@protonmail.com>
2021-10-14 23:29:28 -0500

    Updated Css-Updated File names-Added Buttons

--- web.py
+++ web.py
@@ -3,15 +3,22 @@ from flask import render_template;
 from flask_bootstrap import Bootstrap;
 from markupsafe import escape;
 app = Flask(__name__);
-@app.route("/")
-def index():
-    return "<p>Index Page</p>";
+#@app.route("/")
+#def index():
+#    return "<p>Index Page</p>";
 @app.route("/hello")
 def hello_world():
     return "<p>Hello World</p>";
 @app.route("/<name>")
 def hello(name):
     return f"<p>Hello, {escape(name)}</p>";
-@app.route("/temp")
-def temp():
-    return render_template('temp.html');
+@app.route("/")
+def index():
+    return render_template('home.html');
+@app.route("/bio")
+def bio():
+    return "<p>BIO</p>";
+@app.route("/projects")
+def projects():
+    return "<p>PROJECTS</p>";
+
